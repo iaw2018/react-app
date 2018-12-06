35ca547b49857fd01687ca8fc80cf49ec6b0ddee
Santi <santii.barboza@gmail.com>
2018-12-06 14:44:50 -0300

    add Component PlayersTable

---
+++ src/PlayersTable/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Panel} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import SelectedPlayersSelector	from './selector';
+
+const PlayersTable = (props) => {
+  return (
+      <Table striped bordered condensed >
+      	<thead>
+      		<tr>
+      			<th>Player</th>
+      			<th>Position</th>
+      			<th>Team</th>
+      			<th>Age</th>
+      		</tr>
+      	</thead>
+        <tbody>
+          {props.players.map(player =>
+            <tr key={player.id}>
+              <td>{player.name}</td>
+              <td className="text-right">{player.position}</td>
+              <td className="text-right">{player.team}</td>
+              <td className="text-right">{player.age}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+  )
+}
+const mapStateToProps= state => {
+  return {players:SelectedPlayersSelector(state)};
+}
+
+export default connect(mapStateToProps)(PlayersTable);
